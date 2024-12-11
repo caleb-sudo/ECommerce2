b2bb6c6891a9743a06c2e255bad5c4d189396852
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2024-12-11 10:01:12 -0700

    Create productsPage.js

---
+++ productsPage.js
@@ -0,0 +1,23 @@
+const products = [
+  { name: "Quantum Supremacy", price: 15.99, id: 1, author: "Michio Kaku", pict1: "resources/book1(1).jpg", pict2: "resources/book1(2).jpg", pict3: "resources/book1(3).jpg", pict4: "resources/book1(4).jpg", },
+  { name: "On The Origin Of Time", price: 15.99, id: 2, author: "Thomas Hertog", pict1: "resources/book2(1).jpg", pict2: "resources/book2(2).jpg", pict3: "resources/book2(3).jpg", pict4: "resources/book2(4).jpg", },
+  { name: "Origins", price: 15.99, id: 3, author: "Neil DeGrasse Tyson & Donald Goldsmith", pict1: "resources/book3(1).jpg", pict2: "resources/book3(2).jpg", pict3: "resources/book3(3).jpg", pict4: "resources/book3(4).jpg", },
+  { name: "Astrophysics for people in a hurry", price: 15.99, id: 4, author: "Neil DeGrasse Tyson", pict1: "resources/book4(1).jpg", pict2: "resources/book4(2).jpg", pict3: "resources/book4(3).jpg", pict4: "resources/book4(4).jpg", },
+  { name: "An astronaut's guide to life", price: 10.00, id: 5, author: "Chris Hadfield", pict1: "resources/book5(1).jpg", pict2: "resources/book5(2).jpg", pict3: "resources/book5(3).jpg", pict4: "resources/book5(4).jpg", },
+  { name: "The Grand Design", price: 9.00, id: 6, author: "Stephen Hawking", pict1: "resources/book6(1).jpg", pict2: "resources/book6(2).jpg", pict3: "resources/book6(3).jpg", pict4: "resources/book6(4).jpg", },
+  { name: "Letters from an Astrophysicist", price: 25.00, id: 7, author: "Neil DeGrasse Tyson", pict1: "resources/book7(1).jpg", pict2: "resources/book7(2).jpg", pict3: "resources/book7(3).jpg", pict4: "resources/book7(4).jpg", },
+  { name: "Brief Answers, Big Questions, Collection", price: 45.00, id: 8, author: "Stephen Hawking", pict1: "resources/book8(1).jpg", pict2: "resources/book8(2).jpg", pict3: "resources/book8(3).jpg", pict4: "resources/book8(4).jpg", },
+  { name: "The C Programming Language", price: 15.99, id: 9, author: "Brian W. Kernighan & Dennis M. Ritchie", pict1: "resources/book9(1).jpg", pict2: "resources/book9(2).jpg", pict3: "resources/book9(3).jpg", pict4: "resources/book9(4).jpg", },
+  { name: "The Three Body Problem Trilogy", price: 50.00, id: 10, author: "Cixin Liu", pict1: "resources/book10(1).jpg", pict2: "resources/book10(2).jpg", pict3: "resources/book10(3).jpg", pict4: "resources/book10(4).jpg", },
+  { name: "Supernova Era", price: 15.99, id: 11, author: "Cixin Liu", pict1: "resources/book11(1).jpg", pict2: "resources/book11(2).jpg", pict3: "resources/book11(3).jpg", pict4: "resources/book11(4).jpg", },
+  { name: "Ball Lightning", price: 15.99, id: 12, author: "Cixin Liu", pict1: "resources/book12(1).jpg", pict2: "resources/book12(2).jpg", pict3: "resources/book12(3).jpg", pict4: "resources/book12(4).jpg", },
+  { name: "To Hold Up The Sky", price: 15.99, id: 13, author: "Cixin Liu", pict1: "resources/book13(1).jpg", pict2: "resources/book13(2).jpg", pict3: "resources/book13(3).jpg", pict4: "resources/book13(4).jpg", },
+  { name: "The Wandering Earth", price: 15.99, id: 14, author: "Cixin Liu", pict1: "resources/book14(1).jpg", pict2: "resources/book14(2).jpg", pict3: "resources/book14(3).jpg", pict4: "resources/book14(4).jpg", },
+  { name: "The Redemtion Of Time", price: 20.00, id: 15, author: "Baoshu", pict1: "resources/book15(1).jpg", pict2: "resources/book15(2).jpg", pict3: "resources/book15(3).jpg", pict4: "resources/book15(4).jpg", },
+  { name: "Bowl Of Heaven & Shipstar", price: 15.00, id: 16, author: "Gregory Benford & Larry Niven", pict1: "resources/book16(1).jpg", pict2: "resources/book16(2).jpg", pict3: "resources/book16(3).jpg", pict4: "resources/book16(4).jpg", },
+  { name: "Glourious", price: 15.99, id: 17, author: "Gregory Benford & Larry Niven", pict1: "resources/book17(1).jpg", pict2: "resources/book17(2).jpg", pict3: "resources/book17(3).jpg", pict4: "resources/book17(4).jpg", },
+];
+
+function enterPage(page) {
+  window.location.href = "productPage.html";
+}
