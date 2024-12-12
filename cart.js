4ab58698efe61723fc089cda161c468e038dc01f
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2024-12-12 09:20:32 -0700

    Update cart.js

--- cart.js
+++ cart.js
@@ -18,28 +18,25 @@ const products = [
   { name: "Glourious", price: 15.99, id: 17, quantity: 1, },
 ];
 
-function showCart() {
-  if (CheckBrowser()) {
-    var key = "";
-    var list = "<tr><th>Item</th><th>Value</th></tr>\n";
-    var i = 0;
-    for (i = 0; i <= localStorage.length-1; i++) {
-      key = localStorage.key(i);
-      list += "<tr><td>" + key + "</td>\n<td>" + localStorage.getItem(key) + "</td></tr>\n";
-    }
-    if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
-        list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
-    }
-    document.getElementById('list').innerHTML = list;
-  } else {
-    alert('Cannot save shopping list as your browser does not support HTML 5');
-  }
+const ul = document.querySelector('ul');
+let items = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+items.forEach(addTask);
+function addTask(text) {
+	const li = document.createElement('li');
+	li.textContent = text;
+	ul.appendChild(li);
 }
 
-function CheckBrowser() {
-	if ('localStorage' in window && window['localStorage'] !== null) {
-		return true;
-	} else {
-			return false;
-	}
+function addItem() {
+}
+
+function removeItem() {
+	
+}
+
+function clearCart() {
+	localStorage.clear();
+	ul.innerHTML = '';
+	items = [];
 }
